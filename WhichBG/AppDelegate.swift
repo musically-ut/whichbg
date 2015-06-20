f20cee5c8bb8c96557928d87a30ebf0a1e84ce16
Utkarsh Upadhyay <musically.ut@gmail.com>
2015-06-20 10:34:16 +0200

    Initial Commit

---
+++ WhichBG/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  WhichBG
+//
+//  Created by Utkarsh Upadhyay on 6/20/15.
+//  Copyright (c) 2015 Utkarsh Upadhyay. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ WhichBG/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  WhichBG
+//
+//  Created by Utkarsh Upadhyay on 6/20/15.
+//  Copyright (c) 2015 Utkarsh Upadhyay. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
