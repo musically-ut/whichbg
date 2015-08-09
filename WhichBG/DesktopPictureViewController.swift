d6beb8e5c3aa5debfb1382e297d762b266be3c55
Utkarsh Upadhyay <musically.ut@gmail.com>
2015-08-09 12:26:09 +0200

    Better placement of the error message and wallpapers.
    Also, add some more error handling.

--- WhichBG/DesktopPictureViewController.swift
+++ WhichBG/DesktopPictureViewController.swift
@@ -25,29 +25,32 @@ class DesktopPictureViewController: NSViewController {
         let paths = NSSearchPathForDirectoriesInDomains(.ApplicationSupportDirectory, .UserDomainMask, true)
         if let appSupp = paths.first as? NSString {
             let dbPath = appSupp.stringByAppendingPathComponent("Dock/desktoppicture.db")
-            let db = Database(dbPath)
-            let value = Expression<String?>("value")
-            
-            // TODO: Correct error handling here.
-            // Now by personal obseravtion, there are three kinds of entries in the `data` table.
-            //   1. Folders (e.g. ~/Pictures)
-            //   2. Numbers (e.g. 1)
-            //   3. Absolute paths (e.g. /Library/Desktop Pictures/Yosemite.jpg)
-            //   4. Relative filenames (e.g. 49 - Ei3XhvZ.jpg)
-            //
-            // Our strategy is to figure out the absolute paths and all possible combination of the folders and pictures
-            // which exist and are valid image files. We'll show those items on a Grid and allow the user to open them
-            // in finder using a single click.
-            
-            var filesFolders : [String] = []
-            for row in db["data"] {
-                if let fileFolder = row[value] {
-                    filesFolders.append(fileFolder)
+            if isFile(dbPath) {
+                let db = Database(dbPath)
+                let value = Expression<String?>("value")
+
+                // TODO: This function does no error handling.
+                // Now by personal obseravtion, there are three kinds of entries in the `data` table.
+                //   1. Folders (e.g. ~/Pictures)
+                //   2. Numbers (e.g. 1)
+                //   3. Absolute paths (e.g. /Library/Desktop Pictures/Yosemite.jpg)
+                //   4. Relative filenames (e.g. 49 - Ei3XhvZ.jpg)
+                //
+                // Our strategy is to figure out the absolute paths and all possible combination of the folders and pictures
+                // which exist and are valid image files. We'll show those items on a Grid and allow the user to open them
+                // in finder using a single click.
+                
+                var filesFolders : [String] = []
+                for row in db["data"] {
+                    if let fileFolder = row[value] {
+                        filesFolders.append(fileFolder)
+                    }
                 }
+                return findAllExistingFilesIn(filesFolders)
+            } else {
+                println("The database file was not found.")
+                return nil
             }
-            let files = findAllExistingFilesIn(filesFolders)
-            
-            return files
         } else {
             println("No Application Support directory was returned. Exiting.")
             return nil
@@ -57,50 +60,48 @@ class DesktopPictureViewController: NSViewController {
     @IBOutlet weak var collectionView: NSScrollView!
     @IBOutlet weak var imageCollectionView: NSCollectionView!
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do view setup here.
-        
-        let imageWidth : CGFloat = 200.0, gutter : CGFloat = 10.0
-        var numImages : Int = 0, lastImg : NSImageView? = nil
+    override func viewWillAppear() {
+        super.viewWillAppear()
         
         if let files = self.refreshFiles() {
+            self.imageCollectionView.hidden = false
+            
             // Silently ignoring the files for which data cannot be loaded
             let images = files.map { NSImage(contentsOfFile: $0) }.filter { $0 != nil }.map { $0! }
+            
+            let contentHeight = self.collectionView.contentSize.height
+            let gutter = CGFloat(10)
+            
+            self.imageCollectionView.minItemSize = NSMakeSize((16.0 / 10.0) * contentHeight + gutter, contentHeight)
             self.imageCollectionView.itemPrototype = WallpaperItem()
             self.imageCollectionView.selectable = true
             self.imageCollectionView.content = images
-            
-            self.imageCollectionView!.autoresizingMask = .ViewMaxXMargin | .ViewMinXMargin | .ViewMaxYMargin | .ViewMinYMargin
-            // self.imageCollectionView!.autoresizingMask = NSAutoresizingMaskOptions.ViewWidthSizable | NSAutoresizingMaskOptions.ViewMaxXMargin | NSAutoresizingMaskOptions.ViewMinYMargin | NSAutoresizingMaskOptions.ViewHeightSizable | NSAutoresizingMaskOptions.ViewMaxYMargin
-            
+
             for (idx, img) in enumerate(images) {
                 let item = self.imageCollectionView.itemAtIndex(idx) as? WallpaperItem
                 item!.getView().image = img
             }
         } else {
+            self.imageCollectionView.hidden = true
             let errorLabel = NSTextView(frame: self.collectionView.frame)
-            errorLabel.editable = false
-            errorLabel.string = "Error!"
+            errorLabel.drawsBackground = false
+            errorLabel.alignCenter(nil)
+            errorLabel.string = "Sorry, could not load the wallpapers."
             
+            let fontSize = CGFloat(16), contentSize = self.collectionView.contentSize
+            errorLabel.frame = NSMakeRect(0, (contentSize.height - fontSize) / 2, contentSize.width, fontSize)
+            errorLabel.editable = false
+  
             self.collectionView.addSubview(errorLabel)
-            // TODO: Show that nothing could be shown.
         }
     }
 }
 
 class WallpaperItem : NSCollectionViewItem {
-    
     var wallpaperView: NSImageView?
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-    }
-    
     override func loadView() {
-        // self.wallpaperView = NSImageView(frame: NSZeroRect)
-        let marginX = CGFloat(0.0), marginY = CGFloat(0.0), imageWidth = CGFloat(320.0), imageHeight = CGFloat(240.0)
-        self.wallpaperView = NSImageView(frame: NSMakeRect(marginX, marginY, imageWidth, imageHeight))
+        self.wallpaperView = NSImageView(frame: NSZeroRect)
         self.view = self.wallpaperView!
     }
     
